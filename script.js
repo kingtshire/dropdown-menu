9461963faf14494c534207c8649d26b3cf603158
Tshireletso P. Tshielo <tshielo01@gmail.com>
2023-12-28 16:39:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const mainItems = document.querySelectorAll (
+    '.main-item'
+);
+
+mainItems.forEach((mainItem) => {
+    mainItem.addEventListener('click', () => {
+        mainItem.classList.toggle(
+            'main-item--open'
+        );
+
+    })
+});
